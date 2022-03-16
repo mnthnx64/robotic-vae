39e3982e00c317a1cce9e78a6e4f5958b62bb65e
Manthan M1 <mnthn.ai@gmail.com>
2022-03-16 11:46:20 -0700

    Mapping for arm

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+from pathlib import Path
+import shutil
+
 import torch
 from torchvision import datasets, transforms
 import torch.optim as optim
@@ -6,16 +10,25 @@ from models import VAE
 from torchvision.utils import save_image
 
 BATCH_SIZE = 100
+DATASET_DIR = './dataset/mnist_data/'
+SAVE_DIR = 'runs/train/'
+RESUME = True
+
+if os.path.exists(SAVE_DIR) and not RESUME: shutil.rmtree(SAVE_DIR)
+if not RESUME: os.makedirs(SAVE_DIR)
 
 # MNIST Dataset
-train_dataset = datasets.MNIST(root='./dataset/mnist_data/', train=True, transform=transforms.ToTensor(), download=True)
-test_dataset = datasets.MNIST(root='./dataset/mnist_data/', train=False, transform=transforms.ToTensor(), download=False)
+train_dataset = datasets.MNIST(root=DATASET_DIR, train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = datasets.MNIST(root=DATASET_DIR, train=False, transform=transforms.ToTensor(), download=False)
 
 # Data Loader (Input Pipeline)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
+
 vae = VAE(x_dim=784, h_dim1= 512, h_dim2=256, z_dim=2)
+if RESUME:
+    vae.load_state_dict(torch.load(SAVE_DIR + "model.pth"))
 if torch.cuda.is_available():
     vae.cuda()
 
@@ -44,6 +57,16 @@ def train(epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item() / len(data)))
+    if (epoch % 10) == 0:
+        path = os.path.join(SAVE_DIR, 'model.pth')
+        torch.save(vae.cpu().state_dict(), path) # saving model
+        with torch.no_grad():
+            z = torch.randn(64, 2)
+            sample = vae.decoder(z)
+            
+            save_image(sample.view(64, 1, 28, 28), SAVE_DIR + 'Sample_%s.png' % epoch)
+        if torch.cuda.is_available():
+            vae.cuda()
     print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(train_loader.dataset)))
 
 def test():
@@ -60,12 +83,6 @@ def test():
     test_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
 
-for epoch in range(1, 51):
+for epoch in range(1, 201):
     train(epoch)
     test()
-
-with torch.no_grad():
-    z = torch.randn(64, 2).cuda()
-    sample = vae.decoder(z).cuda()
-    
-    save_image(sample.view(64, 1, 28, 28), './samples/sample_' + '.png')
--- test.py
+++ test.py
@@ -16,13 +16,37 @@ def policy(observation, desired_goal):
 arr = np.zeros((20))
 count = 0
 
+"""
+Array:
+0: X-arm
+1: Y-arm
+2: X-index
+3: Y-index
+4: Z-index
+5: X-middle
+6: Y-middle
+7: Z-middle
+8: X-ring
+9: Y-ring
+10: Z-ring
+11: Palm-pinky displacement
+12: X-pinky
+13: Y-pinky + Y-ring
+14: Z-pinky + Z-ring
+15: Rot-thumb
+16: Y-thumb
+17: X-thumb
+18: Z-thumb.j1
+19: Z-thumb.j2
+"""
+
 while count != 1000:
     action = policy(obs['observation'], obs['desired_goal'])
-    # arr[0] = np.random.randn()
+    # arr[0] = np.random.randn() 
     # arr[1] = np.random.randn()
-    arr[2] = np.random.randn()
-    arr[3] = np.random.randn()
-    arr[4] = np.random.randn()
+    # arr[2] = np.random.randn()
+    arr[12] = np.random.randn()
+    # arr[4] = np.random.randn()
     obs, reward, done, info = env.step(arr)
     env.render()
     # If we want, we can substitute a goal here and re-compute
